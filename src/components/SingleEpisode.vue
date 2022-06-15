36247fc565bc3b9976ff96ad52bca52a8a25f228
enbeeing <eddykjllr@hotmail.com>
2022-06-15 10:33:57 +0200

    global css, + home elements (first draft)

--- src/components/SingleEpisode.vue
+++ src/components/SingleEpisode.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="episode">
-    <h3>{{ episode.title }}</h3>
+    <p class="title">{{ episode.title }}</p>
     <p class="date">{{ episode.date }}</p>
     <p>{{ episode.description }}</p>
     <p>Rating: {{ episode.rating }}</p>
@@ -14,4 +14,23 @@ const props = defineProps({
 </script>
 
 <style>
+.episode {
+  background: var(--light-bg-color);
+  padding: 1.2rem 1rem;
+  margin: 1.2rem;
+  border-radius: 5px;
+  /* shadow? */
+}
+
+.episode > p {
+  padding: 0.25rem;
+}
+
+.episode .date {
+  color: var(--light-text-color);
+}
+
+.episode .title {
+  font-weight: bold;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="home">
-    <EpisodeList :episodes="episodes" />
+    <div v-if="error">{{ error }}</div>
+    <div v-if="episodes.length">
+      <EpisodeList :episodes="episodes" />
+    </div>
+    <div v-else>
+      <p>loading...</p>
+    </div>
+    <button class="add-button">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+    </button>
   </div>
 </template>
 
 <script setup>
-import getEpisodes from "../composables/getEpisodes";
 import EpisodeList from "../components/EpisodeList.vue";
+import getEpisodes from "../composables/getEpisodes";
 
 const { episodes, error, load } = getEpisodes();
 
 load();
 </script>
+
+<style>
+.home .add-button {
+  border: none;
+  outline: none;
+  background: var(--accent-color);
+  border-radius: 50%;
+  max-width: 80px;
+  max-height: 80px;
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+}
+.home svg {
+  color: var(--main-bg-color);
+}
+</style>
